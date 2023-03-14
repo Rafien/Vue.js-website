bd10764c50511f99ba98e0493b4b1d75521838ae
Raf <raphael.choquet@gmail.com>
2023-03-14 17:40:51 +0100

    Added direct link to specific card-types

--- src/components/IndexComponent.vue
+++ src/components/IndexComponent.vue
@@ -1,7 +1,10 @@
 <template>
   <ul>
-    <li><router-link :to="{name: 'card_page', params:{nom: 'Queen Kayla bin-Kroog'}}">Queen Kayla</router-link></li>
-    <li><router-link :to="{name: 'card_page', params:{nom: 'Omniscience'}}">Omniscience</router-link></li>
+    <li><router-link :to="{name: 'card_page', params:{nom: 'Queen Kayla bin-Kroog'}}">Queen Kayla (creature)</router-link></li>
+    <li><router-link :to="{name: 'card_page', params:{nom: 'Omniscience'}}">Omniscience (non-creature)</router-link></li>
+    <li><router-link :to="{name: 'card_page', params:{nom: 'Assure'}}">Assure // Assemble (split-cards)</router-link></li>
+    <li><router-link :to="{name: 'card_page', params:{nom: 'Love struck beast'}}">Love struck beast (adventure-card)</router-link></li>
+    <li><router-link :to="{name: 'card_page', params:{nom: 'Extus, Oriq Overlord'}}">Extus, Oriq Overlord // Awaken the Blood Avatar (transform-card)</router-link></li>
   </ul>
 </template>
 

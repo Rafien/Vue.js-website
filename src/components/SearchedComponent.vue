3c8b0fad74c49d6c36a3011fa1ae3c3a38c11dd5
Raf <raphael.choquet@gmail.com>
2023-04-03 16:35:37 +0200

    fixed all bi-card cards

--- src/components/SearchedComponent.vue
+++ src/components/SearchedComponent.vue
@@ -16,15 +16,14 @@
                 axios.get('https://api.scryfall.com/cards/autocomplete?q=' + this.search).then(
                     (result) => {
                     for ( let i=0; i<result.data.total_values; i++){
-                        console.log(i)
                         axios.get('https://api.scryfall.com/cards/named?fuzzy=' + result.data.data[i]).then(
                         (resultats) => {
-                            console.log(resultats.data.name)
                             this.list_card.push(resultats)
-                            console.log(this.list_card)
+                            console.log(resultats.data.name)
                         }
                     )
                 }
+                console.log(this.list_card)
         }
       )
     }
@@ -35,18 +34,43 @@
 </script>
 
 <template>
-        <div v-for="card in list_card" :key="card.data.image_uris.png" class="card_img card_grid">
-            <span>
-                <RouterLink :to="{ name: 'card_page', params: { nom:card.data.name }}"  ><img :src ="card.data.image_uris.png" ></RouterLink>
-            </span>
-            <span class="card_infos">
-                <div class="card_info">Nom : <span id="nom">{{ card.data.name }}</span> </div>
-                <div v-if=" card.data.mana_cost != 0" class="card_info">Mana Cost : {{ card.data.mana_cost }}</div>
-                <div class="card_info">Type : {{ card.data.type_line }}</div>
-                <div v-if="card.data.oracle_text != ''" class="card_info">Text : {{ card.data.oracle_text }}</div>
-                <div v-if=" card.data.power != undefined" class="card_info">Power : {{ card.data.power }}</div>
-                <div v-if=" card.data.power != undefined" class="card_info">Toughness : {{ card.data.toughness }}</div>
-            </span>
+        <div v-for="card in list_card">
+            
+                <div class="card_grid" v-if="card.data.layout == 'modal_dfc' || card.data.layout == 'transform' || card.data.layout == 'flip'">
+                    <span class="card_img">
+                        <RouterLink :to="{ name: 'card_page', params: { nom:card.data.card_faces[0].name }}"  ><img :src ="card.data.card_faces[0].image_uris.png" ></RouterLink>
+                        
+                    </span>
+                    <span class="card_infos">
+                        <div class="card_info">Nom : <span id="nom">{{ card.data.card_faces[0].name }}</span> </div>
+                        <div v-if=" card.data.card_faces[0].mana_cost != 0" class="card_info">Mana Cost : {{ card.data.card_faces[0].mana_cost }}</div>
+                        <div class="card_info">Type : {{ card.data.card_faces[0].type_line }}</div>
+                        <div v-if="card.data.card_faces[0].oracle_text != ''" class="card_info">Text : {{ card.data.card_faces[0].oracle_text }}</div>
+                        <div v-if=" card.data.card_faces[0].power != undefined" class="card_info">Power : {{ card.data.card_faces[0].power }}</div>
+                        <div v-if=" card.data.card_faces[0].toughness != undefined" class="card_info">Toughness : {{ card.data.card_faces[0].toughness }}</div>
+                    </span>
+                </div>
+                <div v-else class="card_grid">
+                    
+                    <span class="card_img">
+                        <RouterLink :to="{ name: 'card_page', params: { nom:card.data.name }}"  ><img :src ="card.data.image_uris.png" ></RouterLink>
+                        
+                    </span>
+                    <span class="card_infos">
+                        <div v-if="card.data.layout !== 'adventure' && card.data.layout !== 'split'" class="card_info">Nom : <span id="nom">{{ card.data.name }}</span> </div>
+                        <div v-if="card.data.layout == 'adventure' || card.data.layout == 'split'" class="card_info">Nom : {{ card.data.card_faces[0].name }}</div>
+                        <div v-if="card.data.mana_cost != 0 && card.data.layout !== 'adventure' && card.data.layout !== 'split'" class="card_info">Mana Cost : {{ card.data.mana_cost }}</div>
+                        <div v-if="card.data.layout == 'adventure' || card.data.layout == 'split'" class="card_info">Mana Cost : {{ card.data.card_faces[0].mana_cost }}</div>
+                        <div v-if="card.data.layout !== 'adventure' && card.data.layout !== 'split'" class="card_info">Type : {{ card.data.type_line }}</div>
+                        <div v-if="card.data.layout == 'adventure' || card.data.layout == 'split'" class="card_info">Type :  {{ card.data.card_faces[0].type_line }}</div>
+                        <div v-if="card.data.oracle_text != '' && card.data.layout !== 'adventure' && card.data.layout !== 'split'" class="card_info">Text : {{ card.data.oracle_text }}</div>
+                        <div v-if="card.data.layout == 'adventure' || card.data.layout == 'split'" class="card_info">Text : {{ card.data.card_faces[0].oracle_text }}</div>
+                        <div v-if=" card.data.power != undefined" class="card_info">Power : {{ card.data.power }}</div>
+                        <div v-if=" card.data.power != undefined" class="card_info">Toughness : {{ card.data.toughness }}</div>
+                    </span>
+                </div>
+            
+            
             
         </div>
 
